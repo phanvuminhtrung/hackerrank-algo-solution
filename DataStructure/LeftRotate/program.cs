c1d83c786f7b69e51bec452d4b4145fe8a6e1459
Troy Phan <troyphan98@gmail.com>
2024-08-14 11:08:53 +1000

    adding rotate left

---
+++ DataStructure/LeftRotate/program.cs
@@ -0,0 +1,52 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static List<int> rotateLeft(int d, List<int> arr)
+    {
+        int n = arr.Count;
+        // Calculate effective rotations
+        int effectiveRotations = d % n;
+        
+        // Slice the array and concatenate the parts
+        List<int> rotatedArray = arr.Skip(effectiveRotations).Concat(arr.Take(effectiveRotations)).ToList();
+        
+        return rotatedArray;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.rotateLeft(d, arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
